8fd65b20c7bb78c1c81a38c2af6b2a77e018e404
marwan <marwandarwish9@gmail.com>
2020-05-08 16:56:18 +0200

    Our first

---
+++ corona.py
@@ -0,0 +1,59 @@
+#Imports
+import requests
+from bs4 import BeautifulSoup
+#import pandas as pd
+
+class Corona:
+	def __init__(self):
+    #input from user
+		contry = str(input("Enter the contry : "))
+    
+		#Url , response , source , soup  
+		self.url = f"https://www.worldometers.info/coronavirus/country/{contry}/"
+
+		self.response = requests.get(self.url)
+
+		self.source = self.response.content
+
+		self.soup = BeautifulSoup(self.source , "html.parser")
+
+	def findData(self):
+		#Find the div 
+		self.cases = self.soup.find_all("div" , class_="maincounter-number")
+
+		#Data list
+		self.data = []
+
+		#Find the span and get the data from it
+		for i in self.cases:
+			self.span = i.find("span")
+			self.data.append(self.span.string)
+		print(f"total cases is : {self.data[0]}")
+		print(f"total Deaths is : {self.data[1]}")
+		print(f"total Recovered is : {self.data[2]}")
+        
+
+	def exportData(self):
+		#Export the data to cvs file
+		self.df = pd.DataFrame(
+
+
+									{
+
+											"CoronaData" : self.data
+
+									}
+
+
+			)
+
+		self.df.index = ["Cases" , "Deaths" , "Recovered"]
+
+		self.df.to_csv("Corona.csv")
+
+
+
+if __name__ == "__main__":
+	c = Corona()
+	c.findData()
+	# c.exportData()
